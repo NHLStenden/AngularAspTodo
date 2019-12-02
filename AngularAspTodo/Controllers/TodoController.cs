5e4cbd7ea620b7f13cc342bbce36cc1f4c020936
Joris Lops <jorislops@gmail.com>
2019-12-03 00:17:38 +0100

    Simple Todo Example with Angular and Asp.net Core 3.0. First Commit.

---
+++ AngularAspTodo/Controllers/TodoController.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Linq;
+using AngularAspTodo.Data;
+using AngularAspTodo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularAspTodo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TodoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IEnumerable<Todo> Get()
+        {
+            return _db.Todos.ToArray();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var todoToDelete = _db.Todos.Find(id);
+            if (todoToDelete == null)
+            {
+                return NotFound();
+            }
+
+            _db.Remove(todoToDelete);
+            _db.SaveChanges();
+            
+            return Ok(todoToDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Todo todo)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Todos.Add(todo);
+                _db.SaveChanges();
+            }
+
+            return Ok(todo);
+        }
+    }
+}
